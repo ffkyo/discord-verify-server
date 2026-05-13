c7c8607f5853de0714b0ea302ea75482796c84e9
Amber <marceldoro8@gmail.com>
2026-05-14 00:46:37 +0200

    Add files via upload

---
+++ verify bot/web.py
@@ -0,0 +1,10 @@
+import uvicorn
+from app import app
+import os
+
+def start():
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    start()
